f19825506d00c49fbbd930333569c4bb22709932
SawIsaac <aunghtethtet295810@gmai.com>
2023-06-19 13:08:59 +0630

    hein learn website update

--- PROJECT/heinlearn website/script/plugin.js
+++ PROJECT/heinlearn website/script/plugin.js
@@ -26,4 +26,21 @@ var swiper = new Swiper(".courses-slider", {
     },
   });
 
+  var swiper = new Swiper(".logoslider", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    loop:true,
+    autoplay:{
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    speed: 1000, // Duration of slide transition (in milliseconds)
+  effect: 'slide', // Choose the slide effect
+  slideToClickedSlide: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
   
